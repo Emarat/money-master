cad51a80400d341ea6bcc4f75a25ac6ba88f6f80
Emarat <emarathossain00@gmail.com>
2022-02-17 14:29:31 +0600

    js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-
+function userInput(ID) {
+    const userValue = parseFloat(document.getElementById(ID).value);
+    return userValue;
+}
 
 
 document.getElementById('calc').addEventListener('click', function () {
-    const incomeValue = parseFloat(document.getElementById('income').value);
-    const foodValue = parseFloat(document.getElementById('food').value);
-    const rentValue = parseFloat(document.getElementById('rent').value);
-    const clothsValue = parseFloat(document.getElementById('cloths').value);
+    const incomeValue = userInput('income');
+    const foodValue = userInput('food');
+    const rentValue = userInput('rent');
+    const clothsValue = userInput('cloths');
+    // const incomeValue = parseFloat(document.getElementById('income').value);
+    // const foodValue = parseFloat(document.getElementById('food').value);
+    // const rentValue = parseFloat(document.getElementById('rent').value);
+    // const clothsValue = parseFloat(document.getElementById('cloths').value);
     let expenses = document.getElementById('expenses');
-    // expenses = expenses.innerText;
+
 
     const totalExpenses = foodValue + rentValue + clothsValue;
     expenses.innerText = totalExpenses;
 
     let balance = document.getElementById('balance');
-    // balance = balance.innerText;
+
     const remainingBalance = incomeValue - totalExpenses;
     balance.innerText = remainingBalance;
 
